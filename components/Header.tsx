275d9cc5308ad36f903dd83061ad579a793b3a74
ritikdevelop <hrithikrana216@gmail.com>
2024-12-26 11:53:50 +0530

    Add metadata, UI components for Waste Management App

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import React from "react";
+import {
+  Menu,
+  Coins,
+  Leaf,
+  Search,
+  User,
+  ChevronDown,
+  LogIn,
+  LogOut,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Badge } from "./ui/badge";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+
+const clientId = process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID
