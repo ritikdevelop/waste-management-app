7676c469c575a0bd49b54bb789b6a16ee74eab31
ritikdevelop <hrithikrana216@gmail.com>
2024-12-26 16:36:06 +0530

    Enhance configuration and layout for Waste Management App;

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@
 import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { getAvailableRewards, getUserByEmail } from "@/utils/db/actions";
 
 //! Header
 
 //! Sidebar
 
 import { Toaster } from "react-hot-toast";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +22,39 @@ export default function RootLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [totalEarnings, setTotalEarnings] = useState(0);
 
+  useEffect(() => {
+    const fetchTotalEarnings = async () => {
+      try {
+        const userEmail = localStorage.getItem("userEmail");
+        if (userEmail) {
+          const user = await getUserByEmail(userEmail);
+          console.log("user from layout", user);
+
+          if (user) {
+            const availableRewards = (await getAvailableRewards(
+              user.id
+            )) as any;
+            console.log("availableRewards from layout", availableRewards);
+            setTotalEarnings(availableRewards);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching total earnings:", error);
+      }
+    };
+
+    fetchTotalEarnings();
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="min-h-screen bg-gray-50 flex flex-col">
           {/* Header */}
+          <Header
+            onMenuClick={() => setSidebarOpen(!sidebarOpen)}
+            totalEarnings={totalEarnings}
+          />
           <div className="flex flex-1">
             {/* Sidebar */}
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    env: {
+        // Add your env variables here
+        DATABASE_URL: process.env.NEXT_PUBLIC_NEON_PUBLIC_DATABASE_URL,
+        WEB3_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+  
 export default nextConfig;
