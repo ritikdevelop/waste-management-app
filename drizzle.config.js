28675d63cf3e1c655690bbe06fbf3e17e03fedfe
ritikdevelop <hrithikrana216@gmail.com>
2024-12-26 11:54:31 +0530

    Update database configuration and dependencies for Waste Management App

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,12 +1,9 @@
-
 export default {
-    dialect: "postgresql",
-    schema: "./utils/db/schema.ts",
-    out: "./drizzle",
-    dbCredentials: {
-      url: process.env.NEON_PUBLIC_DATABASE_URL,
-      connectionString:
-      process.env.NEON_PUBLIC_DATABASE_URL,
-    },
-  };
-  
+  dialect: "postgresql",
+  schema: "./utils/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_NEON_PUBLIC_DATABASE_URL,
+    connectionString: process.env.NEXT_PUBLIC_NEON_PUBLIC_DATABASE_URL,
+  },
+};
--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,8 +1,7 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
-import {neon} from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
-import * as schema from './schema'
+const sql = neon(process.env.NEXT_PUBLIC_NEON_PUBLIC_DATABASE_URL);
 
-const sql = neon(process.env.NEON_PUBLIC_DATABASE_URL)
-
-export const db = drizzle(sql, {schema});
+export const db = drizzle(sql, { schema });
