977a843bb13835631a5955cd245cb3dd0c611a1d
ritikdevelop <hrithikrana216@gmail.com>
2024-12-27 17:21:14 +0530

    Add additional environment variables for Gemini API and Google Maps

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Add your env variables here
-        DATABASE_URL: process.env.NEXT_PUBLIC_NEON_PUBLIC_DATABASE_URL,
-        WEB3_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID,
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
+  env: {
+    // Add your env variables here
+    DATABASE_URL: process.env.NEXT_PUBLIC_NEON_PUBLIC_DATABASE_URL,
+    WEB3_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID,
+    GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+    GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
 export default nextConfig;
